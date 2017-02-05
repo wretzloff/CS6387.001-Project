0d889c46c6c5fe62ecef7834c0c7b8166d672ff8
Will Retzloff <wretzloff@gmail.com>
2017-02-05 11:23:01 -0600

    Fleshing out logic in the /GetBooksForClass endpoint.

--- Backend/server.js
+++ Backend/server.js
@@ -1,39 +1,71 @@
 var http = require('http');
-const express = require('express')  
+var https = require('https');
+var express = require('express');
+var cheerio = require('cheerio'); 
 const app = express()  
 const port = 3000
 
 app.get('/', (request, response) => 
 { 
-	response.send('UTD Book Exchange')
+	response.send('UTD Book Exchange');
 })
 
 app.post('/', (request, response) => 
 {  
-	response.send('UTD Book Exchange: POST')
+	response.send('UTD Book Exchange: POST');
 })
 
-app.get('/GetBooksForClass', function(req, response) 
+app.get('/GetBooksForClass', (request, response) => 
 {
-	var arr = [];
-	arr.push({
-        name: "STARTING OUT WITH C++ FROM CNTRL (LOOSEPGS)(W/OUT ACCESS)",
-        edition: "8",
-		author: "GADDIS",
-        ISBN: "9780133778816",
-		required_recommended: "required"
-    });
+	var classFromQueryString = request.query.class;
+	var coursebookCookie;
+
+	var optionsForGet = 
+	{
+		host: 'coursebook.utdallas.edu'
+	};
+
+	var callbackForGet = function(resp) 
+	{
+		//Get the value of ptgsessid from the set-cookie header of the response.
+		var setCookieHeader = String(resp.headers["set-cookie"]);
+		var ptgsessid = setCookieHeader.split(";")[0];
+		coursebookCookie = ptgsessid.split("=")[1];
+		
+		//Use this ptgsessid value to send an HTTP POST to get required textbooks.
+		var dataForPost = 'id='+classFromQueryString+'&div=r-2childcontent';
+		var optionsForPost = 
+		{
+			host: 'coursebook.utdallas.edu',
+			path: '/clips/clip-textbooks.zog',
+			method: 'POST',
+			headers: 
+			{
+				'Content-Type' : 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(dataForPost),
+				'Cookie':'PTGSESSID='+coursebookCookie+';'
+			}
+		};
+		
+		var callbackForPost = function(resp)
+		{
+			resp.setEncoding('utf8');
+			resp.on('data', function (chunk) 
+			{
+				response.header("Content-Type",'application/json');
+				response.send(parseBookHTML(chunk));
+			});
+		}
+		
+		//Send the HTTP POST
+		var post_req = https.request(optionsForPost, callbackForPost) ;
+		post_req.write(dataForPost);
+		post_req.end();
+	}
 	
-	arr.push({
-        name: "STARTING OUT WITH C++: FROM CONTROL ETC",
-        edition: "9",
-		author: "GADDIS",
-        ISBN: "9780133769395",
-		required_recommended: "recommended"
-    });
-	
-	response.header("Content-Type",'application/json');
-	response.send(JSON.stringify(arr, null, 4));
+	//Send the HTTP GET
+	var get_req = http.request(optionsForGet, callbackForGet);
+    get_req.end();
 })
 
 app.listen(port, (err) => 
@@ -46,4 +78,26 @@ app.listen(port, (err) =>
   console.log(`server is listening on ${port}`)
 })
 
-
+function parseBookHTML(html)
+{
+	var arr = [];
+	var $ = cheerio.load(html);
+	$('td.textbook').each(function(){
+        var data = $(this);
+		var title = $(".booktitle", data).text();
+		var edition = $($(".author", data)[3]).text();
+		var author = $($(".author", data)[0]).children().eq(0).text();
+		var isbn = $(".isbn", data).children().first().text();
+		var recommended = $($(".author", data)[1]).text();
+		
+		arr.push({
+			name: title,
+			edition: edition,
+			author: author,
+			ISBN: isbn,
+			required_recommended: recommended
+		});
+	})
+	
+	return JSON.stringify(arr, null, 4);
+}
