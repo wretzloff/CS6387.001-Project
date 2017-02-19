afb1602c2734634996d5437f041bd0fc0e5842f6
Will Retzloff <wretzloff@gmail.com>
2017-02-19 15:50:10 -0600

    Testing openshift deployment.

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ var utdtextbookexchange_app = function() {
 			//Insert code here get a third party sale price...................
 			console.log('Insert code here get a third party sale price...................');
 			var thirdPartyPriceInfo = [];
-			thirdPartyPriceInfo.push({price: '51.47', source: 'Amazon', link: 'https://www.amazon.com/Starting-Control-Structures-through-Objects/dp/0133778819/ref=sr_1_1?ie=UTF8&qid=1487534860&sr=8-1&keywords=9780133778816'});
+			thirdPartyPriceInfo.push({price: '51.48', source: 'Amazon', link: 'https://www.amazon.com/Starting-Control-Structures-through-Objects/dp/0133778819/ref=sr_1_1?ie=UTF8&qid=1487534860&sr=8-1&keywords=9780133778816'});
 			response.send(thirdPartyPriceInfo);
 		};
 		
