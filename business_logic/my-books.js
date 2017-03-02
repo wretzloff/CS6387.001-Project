76320e1506c9f9fb679a04e9a3ffe25dfb2763ae
Will Retzloff <wretzloff@gmail.com>
2017-03-01 20:51:23 -0600

    Extracting my-books logic into its own file.

---
+++ business_logic/my-books.js
@@ -0,0 +1,145 @@
+var http 				= require('http');
+var https 				= require('https');
+var cheerio 			= require('cheerio');
+
+var authenticate        = require('./authenticate');
+
+var methods = {};
+
+methods.getMyBooks = function(request, response, connection)
+{
+	//Get the userId provided in the query string. Technically, we don't even need this, because when we decode the authorization token,  
+			//that gives us the internalUserId. We'll use that, since it's more secure.
+			var providedUserId = request.params.userId;
+			var productionOrStubBooks = request.params.productionOrStubBooks;
+			
+			//Define the function that will be called after each call to UTD Coursebook.
+			var booksArray = [];
+			var getBooksForClassCallbackFunction = function(numOfClasses, classNum, classbooks)
+			{
+				booksArray.push({className: classNum, classbooks: classbooks});
+				if(booksArray.length >= numOfClasses)
+				{
+					response.contentType('application/json');
+					response.json(booksArray);
+				}
+			}
+						
+			//Define a function tht will be called after the checkToken() function has finished validating the authorization token.
+			var afterDatabaseQueryCallbackFunction = function(internalUserId){
+				connection.query("SELECT * from dummy_User_Enrollment where internalUserId = '" + internalUserId + "'", function(err, classRows, fields) 
+				{
+					if (!err)
+					{
+						//Loop through each class and fetch the books for that class, and add them to the response.
+						for (var i in classRows) 
+						{
+							var classNumber = classRows[i].enrolledClass + '.' + classRows[i].semester;
+							//If the dummy flag has been set, then just return fake books. Otherwise, carry on and actually make calls to coursebook.
+							if(productionOrStubBooks === 'stub')
+							{
+								var fakeBooksArray = [];
+								fakeBooksArray.push(
+								{
+									bookName: 'STARTING OUT WITH C++ FROM CNTRL (LOOSEPGS)(W/OUT ACCESS)', 
+									bookEdition: 'PH 8th Edition 2015', 
+									bookAuthor: 'GADDIS',
+									bookISBN: '9780133778816',
+									book_required_recommended: 'Required Text'
+								});
+								getBooksForClassCallbackFunction(classRows.length, classNumber, fakeBooksArray);
+							}
+							else
+							{
+								getBooksForClass(classRows.length, classNumber, getBooksForClassCallbackFunction);
+							}
+						}
+					}
+					else
+					{
+						console.log(err);
+						response.status(500).send({success: false, msg: 'Internal Server Error. Please try again later.'});
+					}
+				});
+			}
+			authenticate.checkToken(request, response, afterDatabaseQueryCallbackFunction);
+}
+
+
+function parseBookHTML(html)
+{
+	var arr = [];
+	var $ = cheerio.load(html);
+	$('td.textbook').each(function(){
+        var data = $(this);
+		var title = $(".booktitle", data).text();
+		var edition = $($(".author", data)[3]).text();
+		var author = $($(".author", data)[0]).children().eq(0).text();
+		var isbn = $(".isbn", data).children().first().text();
+		var recommended = $($(".author", data)[1]).text();
+		
+		arr.push({
+			bookName: title,
+			bookEdition: edition,
+			bookAuthor: author,
+			bookISBN: isbn,
+			book_required_recommended: recommended
+		});
+	})
+	
+	return JSON.stringify(arr, null, 4);
+}
+
+function getBooksForClass(numOfClasses, classNumber, callbackFunction) 
+{
+	var coursebookCookie;
+	var optionsForGet = 
+	{
+		host: 'coursebook.utdallas.edu'
+	};
+
+	var callbackForGet = function(resp) 
+	{
+		//Get the value of ptgsessid from the set-cookie header of the response.
+		var setCookieHeader = String(resp.headers["set-cookie"]);
+		var ptgsessid = setCookieHeader.split(";")[0];
+		coursebookCookie = ptgsessid.split("=")[1];
+
+		//Use this ptgsessid value to send an HTTP POST to get required textbooks.
+		var dataForPost = 'id='+classNumber+'&div=r-2childcontent';
+		var optionsForPost = 
+		{
+			host: 'coursebook.utdallas.edu',
+			path: '/clips/clip-textbooks.zog',
+			method: 'POST',
+			headers: 
+			{
+				'Content-Type' : 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(dataForPost),
+				'Cookie':'PTGSESSID='+coursebookCookie+';'
+			}
+		};
+		
+		var callbackForPost = function(resp)
+		{
+			resp.setEncoding('utf8');
+			resp.on('data', function (chunk) 
+			{
+				var formattedResults = parseBookHTML(chunk);
+				callbackFunction(numOfClasses, classNumber, formattedResults);
+			});
+		}
+	
+		//Send the HTTP POST
+		var post_req = https.request(optionsForPost, callbackForPost) ;
+		post_req.write(dataForPost);
+		post_req.end();
+	}
+
+	//Send the HTTP GET
+	var get_req = http.request(optionsForGet, callbackForGet);
+	get_req.end();
+};
+
+
+module.exports = methods;
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 #!/bin/env node
 var express 			= require('express');
 var fs      			= require('fs');
-var http 				= require('http');
-var https 				= require('https');
-var cheerio 			= require('cheerio');
 var mysql   			= require('mysql');
 var passport			= require('passport');
-var jwt         		= require('jwt-simple');
 var argv 				= require('minimist')(process.argv.slice(2));
 var swagger 			= require("swagger-node-express");
 var bodyParser 			= require('body-parser');
 var authenticate		= require('./business_logic/authenticate');
 var forSaleEntries		= require('./business_logic/forSaleEntries');
+var myBooks				= require('./business_logic/my-books');
 var server_port 		= process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address 	= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var mysql_port 			= '3306';
@@ -47,61 +44,7 @@ var utdtextbookexchange_app = function() {
 		
 		self.getRoutes['/my-books/:userId/:productionOrStubBooks'] = function(request, response) 
 		{
-			//Get the userId provided in the query string. Technically, we don't even need this, because when we decode the authorization token,  
-			//that gives us the internalUserId. We'll use that, since it's more secure.
-			var providedUserId = request.params.userId;
-			var productionOrStubBooks = request.params.productionOrStubBooks;
-			
-			//Define the function that will be called after each call to UTD Coursebook.
-			var booksArray = [];
-			var getBooksForClassCallbackFunction = function(numOfClasses, classNum, classbooks)
-			{
-				booksArray.push({className: classNum, classbooks: classbooks});
-				if(booksArray.length >= numOfClasses)
-				{
-					response.contentType('application/json');
-					response.json(booksArray);
-				}
-			}
-						
-			//Define a function tht will be called after the checkToken() function has finished validating the authorization token.
-			var afterDatabaseQueryCallbackFunction = function(internalUserId){
-				connection.query("SELECT * from dummy_User_Enrollment where internalUserId = '" + internalUserId + "'", function(err, classRows, fields) 
-				{
-					if (!err)
-					{
-						//Loop through each class and fetch the books for that class, and add them to the response.
-						for (var i in classRows) 
-						{
-							var classNumber = classRows[i].enrolledClass + '.' + classRows[i].semester;
-							//If the dummy flag has been set, then just return fake books. Otherwise, carry on and actually make calls to coursebook.
-							if(productionOrStubBooks === 'stub')
-							{
-								var fakeBooksArray = [];
-								fakeBooksArray.push(
-								{
-									bookName: 'STARTING OUT WITH C++ FROM CNTRL (LOOSEPGS)(W/OUT ACCESS)', 
-									bookEdition: 'PH 8th Edition 2015', 
-									bookAuthor: 'GADDIS',
-									bookISBN: '9780133778816',
-									book_required_recommended: 'Required Text'
-								});
-								getBooksForClassCallbackFunction(classRows.length, classNumber, fakeBooksArray);
-							}
-							else
-							{
-								getBooksForClass(classRows.length, classNumber, getBooksForClassCallbackFunction);
-							}
-						}
-					}
-					else
-					{
-						console.log(err);
-						response.status(500).send({success: false, msg: 'Internal Server Error. Please try again later.'});
-					}
-				});
-			}
-			authenticate.checkToken(request, response, afterDatabaseQueryCallbackFunction);
+			myBooks.getMyBooks(request, response, connection);
         };
 		
 		self.getRoutes['/forSaleEntries/isbn/:isbn'] = function(request, response) 
@@ -350,82 +293,6 @@ var utdtextbookexchange_app = function() {
         });
     };
 
-};
-
-
-function parseBookHTML(html)
-{
-	var arr = [];
-	var $ = cheerio.load(html);
-	$('td.textbook').each(function(){
-        var data = $(this);
-		var title = $(".booktitle", data).text();
-		var edition = $($(".author", data)[3]).text();
-		var author = $($(".author", data)[0]).children().eq(0).text();
-		var isbn = $(".isbn", data).children().first().text();
-		var recommended = $($(".author", data)[1]).text();
-		
-		arr.push({
-			bookName: title,
-			bookEdition: edition,
-			bookAuthor: author,
-			bookISBN: isbn,
-			book_required_recommended: recommended
-		});
-	})
-	
-	return JSON.stringify(arr, null, 4);
-}
-
-function getBooksForClass(numOfClasses, classNumber, callbackFunction) 
-{
-	var coursebookCookie;
-	var optionsForGet = 
-	{
-		host: 'coursebook.utdallas.edu'
-	};
-
-	var callbackForGet = function(resp) 
-	{
-		//Get the value of ptgsessid from the set-cookie header of the response.
-		var setCookieHeader = String(resp.headers["set-cookie"]);
-		var ptgsessid = setCookieHeader.split(";")[0];
-		coursebookCookie = ptgsessid.split("=")[1];
-
-		//Use this ptgsessid value to send an HTTP POST to get required textbooks.
-		var dataForPost = 'id='+classNumber+'&div=r-2childcontent';
-		var optionsForPost = 
-		{
-			host: 'coursebook.utdallas.edu',
-			path: '/clips/clip-textbooks.zog',
-			method: 'POST',
-			headers: 
-			{
-				'Content-Type' : 'application/x-www-form-urlencoded',
-				'Content-Length': Buffer.byteLength(dataForPost),
-				'Cookie':'PTGSESSID='+coursebookCookie+';'
-			}
-		};
-		
-		var callbackForPost = function(resp)
-		{
-			resp.setEncoding('utf8');
-			resp.on('data', function (chunk) 
-			{
-				var formattedResults = parseBookHTML(chunk);
-				callbackFunction(numOfClasses, classNumber, formattedResults);
-			});
-		}
-	
-		//Send the HTTP POST
-		var post_req = https.request(optionsForPost, callbackForPost) ;
-		post_req.write(dataForPost);
-		post_req.end();
-	}
-
-	//Send the HTTP GET
-	var get_req = http.request(optionsForGet, callbackForGet);
-	get_req.end();
 };
 
 
