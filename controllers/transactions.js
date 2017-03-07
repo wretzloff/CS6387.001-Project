2f2b5f8896da69c10b40dae2715f295cb16ecca1
Will Retzloff <wretzloff@gmail.com>
2017-03-07 17:12:00 -0600

    Adjusting the return JSON of the transactions/userId/{userId} and transactions/transaction/{transactionId} endpoints

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -208,7 +208,7 @@ methods.getTransactionsByUser = function(request, response, connection)
 		//Join the Transaction table to the ForSale table to get information about the book being bought/sold, like the book title, author, price, etc.
 		var transactionsArray = [];
 		transactionsArray.push({iD: '3', buyerOrSeller: 'seller', buyer_Nickname: 'Daren C', buyer_InternalUserId: '2', transactionDateTime: '2017-02-22 00:02:40', satus: 'Pending', conversationId: 1,  title: 'Software Engineering for Dummies', author: 'Wallace Wang', ISBN: '9780470108543', price: '32.67'});
-		transactionsArray.push({iD: '8', buyerOrSeller: 'buyer', seller_Nickname: 'Jonathan R', seller_InternalUserId: '5', transactionDateTime: '2017-02-24 00:07:41', satus: 'Completed', conversationId: 2,  title: 'Intermediate Algebra', author: 'Alan S. Tussy', ISBN: '9781111567675', price: '88.00'});
+		transactionsArray.push({iD: '8', buyerOrSeller: 'buyer', seller_Nickname: 'Jonathan R', seller_InternalUserId: '5', transactionDateTime: '2017-02-24 00:07:41', satus: 'Pending', conversationId: 2,  title: 'Intermediate Algebra', author: 'Alan S. Tussy', ISBN: '9781111567675', price: '88.00'});
 		response.send(transactionsArray);
 	}
 	
