e925edfb731f3145c1106ba01e9fb6e5f55abcf3
Will Retzloff <wretzloff@gmail.com>
2017-03-07 17:10:13 -0600

    Adjusting the return JSON of the transactions/userId/{userId} and transactions/transaction/{transactionId} endpoints

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -207,8 +207,8 @@ methods.getTransactionsByUser = function(request, response, connection)
 		//Insert code here to select all Transactions for this internalUserId where the transaction status is 0 (for Pending).
 		//Join the Transaction table to the ForSale table to get information about the book being bought/sold, like the book title, author, price, etc.
 		var transactionsArray = [];
-		transactionsArray.push({iD: '3', buyerOrSeller: 'seller', buyer_Nickname: 'Daren C', buyer_InternalUserId: '2', transactionDateTime: '2017-02-22 00:02:40', title: 'Software Engineering for Dummies', author: 'Wallace Wang', ISBN: '9780470108543', price: '32.67'});
-		transactionsArray.push({iD: '8', buyerOrSeller: 'buyer', seller_Nickname: 'Jonathan R', seller_InternalUserId: '5', transactionDateTime: '2017-02-24 00:07:41', title: 'Intermediate Algebra', author: 'Alan S. Tussy', ISBN: '9781111567675', price: '88.00'});
+		transactionsArray.push({iD: '3', buyerOrSeller: 'seller', buyer_Nickname: 'Daren C', buyer_InternalUserId: '2', transactionDateTime: '2017-02-22 00:02:40', satus: 'Pending', conversationId: 1,  title: 'Software Engineering for Dummies', author: 'Wallace Wang', ISBN: '9780470108543', price: '32.67'});
+		transactionsArray.push({iD: '8', buyerOrSeller: 'buyer', seller_Nickname: 'Jonathan R', seller_InternalUserId: '5', transactionDateTime: '2017-02-24 00:07:41', satus: 'Completed', conversationId: 2,  title: 'Intermediate Algebra', author: 'Alan S. Tussy', ISBN: '9781111567675', price: '88.00'});
 		response.send(transactionsArray);
 	}
 	
@@ -225,7 +225,7 @@ methods.getTransactionById = function(request, response, connection)
 		//Insert code here to select from the Transaction table where the transaction ID is providedTransactionId.
 		//...
 		
-		response.send({iD: '3', buyerOrSeller: 'seller', buyer_Nickname: 'Daren C', buyer_InternalUserId: '2', transactionDateTime: '2017-02-22 00:02:40', title: 'Software Engineering for Dummies', author: 'Wallace Wang', ISBN: '9780470108543', price: '32.67'});
+		response.send({iD: '3', buyerOrSeller: 'seller', buyer_Nickname: 'Daren C', buyer_InternalUserId: '2', transactionDateTime: '2017-02-22 00:02:40', satus: 'Pending', conversationId: 1, title: 'Software Engineering for Dummies', author: 'Wallace Wang', ISBN: '9780470108543', price: '32.67'});
 	}
 	
 	authenticate.checkToken(request, response, afterCheckTokenCallback);
