db5fc49ffb5d7249d138b10ff6cc4aad28d4ea63
Will Retzloff <wretzloff@gmail.com>
2017-03-15 17:54:39 -0500

    Updating the /ForSaleEntries endpoints to also return the date that the record was posted..

--- controllers/forSaleEntries.js
+++ controllers/forSaleEntries.js
@@ -120,7 +120,7 @@ methods.postBookForSale = function(request, response, connection)
 
 function convertForSaleEntryRowToJson(row)
 {
-	return {forSaleId: row.forSaleId, isbn: row.isbn, author: row.author, price: row.price, description: row.description, condition: row.bookCondition, conditionDescription: row.bookConditionDescription, seller_nickname: row.nickname};
+	return {forSaleId: row.forSaleId, datePosted: row.postedDateTime, isbn: row.isbn, author: row.author, price: row.price, description: row.description, condition: row.bookCondition, conditionDescription: row.bookConditionDescription, seller_nickname: row.nickname};
 }
 
 module.exports = methods;
