1ce721825a7e3391b99555a70b95ae744f19942b
Will Retzloff <wretzloff@gmail.com>
2017-04-13 20:54:10 -0500

    Test cleanup

--- test/test-salePrice.js
+++ test/test-salePrice.js
@@ -8,39 +8,89 @@ require('mocha-steps');
 var config = require('./testConfig.json');
 var host = config.host;
 var token = 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.Ng.Br3DB77C4acCJ7vdYG-0Lx55oCn80KR4gzV-lPYlvz4';
-var wrongtoken='JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.Ng.Br3DB77C4acCJ7vdYG-0Lx55oCn80KR4gzV-lPYlvz3';
-var isbn_13='9780133778816';
+var test_isbn='9780133778816';
 
 
 describe('GET /salePrice/thirdParty/isbn/', () => {
-  it('should return list price', (done) => {
-    chai.request(host)
-    .get('/salePrice/thirdParty/isbn/'+isbn_13)
-    .set('authorization', token)
-    .end((err, res) => {
-      //console.log(res);
-     should.not.exist(err);
-      res.should.have.status(200); 
-      res.type.should.eql('application/json');
-      res.body.should.include.keys('price','source','link');
-      done();
+	var response;
+	step("HTTP response should be 200",function(done) {
+		chai.request(host)
+    	    .get('/salePrice/thirdParty/isbn/'+test_isbn)
+    	    .set('authorization', token)
+    	    .end((err, res) => {
+				if(res.status != 200)
+				{
+					console.log('\tsuccess: ' + res.body.success);
+					console.log('\tmsg: ' + res.body.msg);
+				}
+				response = res;
+				response.should.have.status(200);
+				done();
+    	    });
     });
-  });
-}).timeout(5000);
+	  
+	step('Response type should be: application/json',function(done) {
+    	response.type.should.eql('application/json');
+		done(); 
+    });
+	
+	step('Response body should include: "price"',function(done) {
+		response.body.should.include.keys('price');
+		done(); 
+    });
+	
+	step('Response body should include: "source"',function(done) {
+		response.body.should.include.keys('source');
+		done(); 
+    });
+	
+	step('Response body should include: "link"',function(done) {
+		response.body.should.include.keys('link');
+		done(); 
+    });
+});
 
 
 describe('GET /salePrice/suggested/isbn/', () => {
-	  it('should return suggest price', (done) => {
-	    chai.request(host)
-	    .get('/salePrice/suggested/isbn/'+isbn_13)
-	    .set('authorization', token)
-	    .end((err, res) => {
-	      should.not.exist(err);
-	      res.status.should.eql(200);
-	      res.type.should.eql('application/json');
-	      res.body.should.include.keys('suggestSalePrice','isbn','reason');
-	      res.body.isbn.should.eql(isbn_13);
-	      done();
-	    });
-	  });
-	}).timeout(5000);
+	var response;
+	step("HTTP response should be 200",function(done) {
+		chai.request(host)
+    	    .get('/salePrice/suggested/isbn/'+test_isbn)
+    	    .set('authorization', token)
+    	    .end((err, res) => {
+				if(res.status != 200)
+				{
+					console.log('\tsuccess: ' + res.body.success);
+					console.log('\tmsg: ' + res.body.msg);
+				}
+				response = res;
+				response.should.have.status(200);
+				done();
+    	    });
+    });
+	
+	step('Response type should be: application/json',function(done) {
+    	response.type.should.eql('application/json');
+		done(); 
+    });
+	
+	step('Response body should include: "suggestSalePrice"',function(done) {
+		response.body.should.include.keys('suggestSalePrice');
+		done(); 
+    });
+	
+	step('Response body should include: "isbn"',function(done) {
+		response.body.should.include.keys('isbn');
+		done(); 
+    });
+	
+	step('Response body "isbn" should be ' + test_isbn,function(done) {
+		response.body.isbn.should.eql(test_isbn);
+		done(); 
+    });
+	
+	step('Response body should include: "reason"',function(done) {
+		response.body.should.include.keys('reason');
+		done(); 
+    });
+});
